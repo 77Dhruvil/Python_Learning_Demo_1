6068ee771f022aefaf9a528343c0b4c58c0037ea
77Dhruvil <pateldhruvil2119976@gmail.com>
2024-06-05 22:21:37 +0530

    Variables and Keywords Day 2 covered all lab excersice

--- EX-Lab05062024/Variables and Keywords.py
+++ EX-Lab05062024/Variables and Keywords.py
@@ -1,16 +1,46 @@
-print ("Hello World")
+print("Hello World")
+print("Hello World")
 
-print (2+2)
-print (2-2)
-print (2*2)
-print (2/2)
+print(2 + 2)
+print(2 - 2)
+print(2 * 2)
+print(2 / 2)
 
-print ("Hello" , "My name is Dhruvil " , sep= "-" )
-print("Dhruvil , is , the , king " , "007" , sep= "-" , end= "\n")
-age = (25)
+print()
+# self
+# args
+# sep=''
+# end='\n'
+# file=None
+
+
+print("Hello", "My name is Dhruvil ", 123, 3.14, True, sep='/')
+print('Hi', 'Dhruvil', 'Patel', sep='-',end='\t')
+print('Hi', 'Dhruvil', 'Patel')
+
+
+print("I am a good person" , end='_')
+print("I am a bad person")
+
+# Program to find the max in arg
+print(max(1, 2, 3, 4, 5))
+print(max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10 , 345 , 900000 , -100 , 1000000.34 ))
+
+#Dynamically typed language
+age = 25
+pi = 3.14
+#Varialble name = variable value
+
+#Types which are supported in python
+
+#int, float, str, bool, None
+
+#int = whole number (1,2,3,456,44.55,-345,-11)
+#float = decimal number(3.14,5.666666,-6.7890)
+#str = string('Dhruvil', 'Patel', 'Hello World')
+#bool = True or False (True, False)
+#None = null value
 print(type(age))
-
-
 
 name = ("Dhruvil Patel")
 print(type(name))
@@ -19,6 +49,17 @@ isDhruvil = (True)
 isDhruvilpatel = (False)
 print(type(isDhruvil))
 print(type(isDhruvilpatel))
+print(type(pi))
+
+#python complex number  - i iota -
+complex_number = (3+4j)
+#Real = 3
+#imaginary = 4
+print(complex_number.real)
+print(complex_number.imag)
 
 
-
+first_name = input("Enter your first name")
+last_name = input("Enter your last name")
+print("Your first name is " , first_name)
+print("Your last name is ", last_name)
