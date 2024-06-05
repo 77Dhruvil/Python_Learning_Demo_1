a5979e255ec202a3e8ff4f50b68cd347f884d21c
77Dhruvil <pateldhruvil2119976@gmail.com>
2024-06-05 08:53:52 +0530

    Variables and Keywords

---
+++ EX-Lab05062024/Variables and Keywords.py
@@ -0,0 +1,24 @@
+print ("Hello World")
+
+print (2+2)
+print (2-2)
+print (2*2)
+print (2/2)
+
+print ("Hello" , "My name is Dhruvil " , sep= "-" )
+print("Dhruvil , is , the , king " , "007" , sep= "-" , end= "\n")
+age = (25)
+print(type(age))
+
+
+
+name = ("Dhruvil Patel")
+print(type(name))
+
+isDhruvil = (True)
+isDhruvilpatel = (False)
+print(type(isDhruvil))
+print(type(isDhruvilpatel))
+
+
+
