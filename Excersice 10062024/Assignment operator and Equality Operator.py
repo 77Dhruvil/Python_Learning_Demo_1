4ef774fff8ac7299cb321e6526c914c3f6740f6b
77Dhruvil <pateldhruvil2119976@gmail.com>
2024-06-10 22:51:40 +0530

    Excersice (Task)

---
+++ Excersice 10062024/Assignment operator and Equality Operator.py
@@ -0,0 +1,17 @@
+# = is the Assignment operator
+x=5
+print(x)
+
+name = "Amit"
+print(name)
+
+# == Equal to operator
+x = 5
+y = 10
+
+print(x == y)
+
+name = "Devil"
+name1 = "Devil"
+print(name == name1)
+
---
+++ Excersice 10062024/power Operator.py
@@ -0,0 +1,5 @@
+#** is the power operator
+a= (2**2)
+b= (3**5)
+print(a)
+print(b)
