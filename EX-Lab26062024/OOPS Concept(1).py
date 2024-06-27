d537fdbf06ad84630abf216f4c2d422402ecd7b1
77Dhruvil <pateldhruvil2119976@gmail.com>
2024-06-27 21:56:51 +0530

    OOPs concept Constructor

--- EX-Lab26062024/OOPS Concept(1).py
+++ EX-Lab26062024/OOPS Concept(1).py
@@ -81,23 +81,50 @@
 #     name = None
 #     id = None
 #
-#     def __init__(self,name):
-#         self.name = name
-#         self.id = id()
+#     def __init__(self, name,id):
+#             self.name = name
+#             self.id = id
+#
 #
 #     #Constructor
 #     # USe the initialize the values for the attributes
 #
 #     def sleep(self):
-#         print("I am sleeping")
+#         print("I am sleeping"+self.name)
 #
-# dog1 = Dog()
-# print(dog1.name)
-# dog1.name = "Tom"
-# print(dog1.name)
+# dog1 = Dog("Chow","1")
+# dog2 = Dog("Puppy", "2")
+#
+# print(f'{dog1.name} has ID {dog1.id}')
+# print(f'{dog2.name} has ID {dog2.id}')
+#
 # dog1.sleep()
-#
-# dog2 = Dog()
-# print(dog2.name)
-# dog2.name = "Scooby"
 # dog2.sleep()
+#
+# class Calc:
+#
+#     def sum(self, a, b):
+#         return a + b
+#     def sub(self, a, b):
+#         return a - b
+#     def mul(self, a, b):
+#         return a * b
+#     def div(self, a, b):
+#         return a / b
+#
+# object_ref = Calc()
+# output = object_ref.sum(10, 20)
+# print(output)
+
+class Student:
+
+    def __init__(self):
+        self.name = input("Enter your name: ")
+        self.age = int(input("Enter your age: "))
+
+    def display(self):
+        print(f"Name is {self.name}, Age is {self.age}")
+
+
+student = Student()
+student.display()
